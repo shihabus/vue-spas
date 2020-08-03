b363ad129146697d32d516d68147d6fdc1c6b63c
Shihab US <shihabudheen@dunzo.in>
2020-08-03 15:54:31 +0530

    adding navigation

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,19 @@
 <template>
   <div id="app">
+    <Navigation />
     <router-view class="container" />
   </div>
 </template>
+
+<script>
+import Navigation from "@/components/Navigation.vue";
+export default {
+  name: "Root",
+  components: {
+    Navigation,
+  },
+};
+</script>
 
 <style lang="scss">
 @import "node_modules/bootstrap/scss/bootstrap";
