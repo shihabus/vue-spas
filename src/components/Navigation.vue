90802f00ff304c52a59b3fa2e73faf8339ade12d
Shihab US <shihabudheen@dunzo.in>
2020-08-03 16:51:34 +0530

    adding navbar style

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,3 +1,20 @@
 <template>
-  <div>Navigation</div>
+  <div>
+    <nav class="navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Meeting Log</router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link class="nav-item nav-link" to="/meetings"
+            >Meetings</router-link
+          >
+          <router-link class="nav-item nav-link" to="/login"
+            >Log in</router-link
+          >
+          <router-link class="nav-item nav-link" to="/register"
+            >Register</router-link
+          >
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
